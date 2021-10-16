dad93804cc9be12aaa5a2572663f20d4d576cd0e
Ryker Cutler <rykercutler@Heidis-MacBook-Pro.local>
2021-10-16 10:31:59 -0600

    We made the gun have 18 rounds and have to press R to reload

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-console.log("test 1 2 3 4")
+let numberDartLeft = 18;
 
 function fireDart() {
-	const newDart = document.createElement("img");
-	newDart.src = "./assets/elite-dart.png"
-	newDart.classList.add("nerfdart");
-	newDart.classList.add("dart-starting-position");
-
-	const bloodSplatterElement = document.createElement("img");
-	bloodSplatterElement.src = "./assets/bloodsplatter.png";
-	bloodSplatterElement.classList.add("bloodsplatter");
-	bloodSplatterElement.classList.add("hidden");
-	setTimeout(function(){
-		bloodSplatterElement.classList.remove("hidden");
-		newDart.classList.remove("dart-starting-position");
-	}, 300);
-	setTimeout(function(){
-		bloodSplatterElement.classList.add("fade");
-	},350)
-
-
-	const imageContainer = document.querySelector(".image-container");
+	if (numberDartLeft > 0) {
 	
-	imageContainer.append(bloodSplatterElement);
-	imageContainer.append(newDart);
+		const newDart = document.createElement("img");
+		newDart.src = "./assets/elite-dart.png"
+		newDart.classList.add("nerfdart");
+		newDart.classList.add("dart-starting-position");
+
+		const bloodSplatterElement = document.createElement("img");
+		bloodSplatterElement.src = "./assets/bloodsplatter.png";
+		bloodSplatterElement.classList.add("bloodsplatter");
+		bloodSplatterElement.classList.add("hidden");
+		setTimeout(function(){
+			bloodSplatterElement.classList.remove("hidden");
+			newDart.classList.remove("dart-starting-position");
+		}, 300);
+		setTimeout(function(){
+			bloodSplatterElement.classList.add("fade");
+		},350)
 
 
+		const imageContainer = document.querySelector(".image-container");
+		
+		imageContainer.append(bloodSplatterElement);
+		imageContainer.append(newDart);
+
+		numberDartLeft = numberDartLeft - 1;
+
+	}
 
 }
+
+function reload() {
+	console.log("is reloading");
+	numberDartLeft = 18;
+}
+
+document.onkeypress = function (e) {
+    e = e || window.event;
+    
+    // when you hit r it reloads after 18 shots
+    if (e.keyCode  === 114) {
+		console.log(e.keyCode);
+		reload()
+    } 
+};
+
