f60543eaf8dc71db5c77029d12d98483861d13a9
Ryker Cutler <rykercutler@gmail.com>
2022-01-11 17:41:17 -0700

    Add timer

--- src/more.js
+++ src/more.js
@@ -1,8 +1,52 @@
 let score = 0;
+let disableGame = false
 function hitZombie() {
-	console.log("Testing")
-	const scoreElement = document.querySelector(".score")
-	console.log(scoreElement)
-	score += 100;
-	scoreElement.innerHTML = score;
+	if (!disableGame) {	
+		console.log("Testing")
+		const scoreElement = document.querySelector(".score")
+		console.log(scoreElement)
+		score = score + 100;
+		scoreElement.innerHTML = score;
+	}
+	console.log("IT WORRRRRRKS")
 }
+let timer = 100;
+const id = setInterval(function(){
+	const timerElement = document.querySelector(".timer")
+	timer = timer - 1
+	timerElement.innerHTML = timer;
+	if (timer <= 0){
+		clearInterval(id);
+		disableGame = true;
+		const zombieElement = document.querySelector(".zombie")
+		zombieElement.style.animation = "none"
+		alert("You Lose your score is " + score)
+	}
+}, 1000)
+
+function addTime(time) {
+	timer += time;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
